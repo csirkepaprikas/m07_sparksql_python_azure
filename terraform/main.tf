e97371df296cd56d79d225d4bde76ad44f6c3537
csirkepaprikas <150279587+csirkepaprikas@users.noreply.github.com>
2025-03-11 22:13:49 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,59 +1,12 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
-}
-
-provider "databricks" {
-  host  = var.DATABRICKS_HOST
-  token = var.DATABRICKS_TOKEN
-}
-
-resource "databricks_notebook" "Azure_Spark_SQL" {
-  path     = "/Users/balage330@gmail.com/Azure_Spark_SQL"
-  language = "SQL"
-  source   = ""
-}
-
-resource "databricks_cluster" "example" {
-  cluster_name = "Azure_Spark_SQL_cluster"
-  spark_version = "7.3.x-scala2.12"
-  node_type_id  = "Standard_DS3_v2"
-  autotermination_minutes = 30
-  num_workers = 1
-}
-
-resource "azurerm_storage_account" "Azure_Spark_SQL_storage" {
-  name                     = var.STORAGE_ACCOUNT_NAME
-  resource_group_name       = var.RESOURCE_GROUP_NAME
-  location                 = "West Europe"
-  account_tier              = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "data" {
-  name                  = "data"
-  storage_account_name  = azurerm_storage_account.Azure_Spark_SQL_storage.name
-  container_access_type = "container"
-}
-
-resource "databricks_job" "run_hotel_weather_query" {
-  name = "run_hotel_weather_query"
-
-  new_cluster {
-    spark_version = "15.4.x-scala2.12"
-    node_type_id  = "Standard_DS3_v2"
-    num_workers   = 2
-  }
-
-  notebook_task {
-    notebook_path = databricks_notebook.Azure_Spark_SQL.path
-
-  }
-
-  schedule {
-    quartz_cron_expression = "0 0 1 * ? *"
-    timezone_id = "UTC"
-  }
-}
-
