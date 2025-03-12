9953148aa9c6f38f4792a8f990db02708c33ceea
csirkepaprikas <150279587+csirkepaprikas@users.noreply.github.com>
2025-03-12 11:49:21 +0100

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "AZURE_SUBSCRIPTION_ID" {
+  type = string
+}
+
+variable "AZURE_TENANT_ID" {
+  type = string
+}
+
+variable "AZURE_CLIENT_ID" {
+  type = string
+}
+
+variable "AZURE_CLIENT_SECRET" {
+  type = string
+}
+
+variable "DATABRICKS_HOST" {
+  type = string
+}
+
+variable "DATABRICKS_TOKEN" {
+  type = string
+}
